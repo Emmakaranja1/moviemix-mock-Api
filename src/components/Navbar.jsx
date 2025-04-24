14959fcd7c1c825967471232d3ab1f718cdd0638
sudeis <ahmedsudeis24@gmail.com>
2025-04-24 10:24:51 +0300

    Improve navbar styling and fix CSS import path in main.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
       <Link to="/Homepage">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
