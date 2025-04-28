e5222ef754a090c1deb0fdec610026d0af74c2dc
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-04-28 12:09:06 +0300

    Complete movie website implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,42 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 //import Home_icon from '../../assets/Home-icon.png';
 //import Logo_icon from '../../assets/Logo-icon.png';
-//import all_movies_icon from '../../assets/All-movies-icon.png'; 
+//import all_movies_icon from '../../assets/All-movies-icon.png';
 //import rated_5_stars from '../../assets/Rating-icon.png';
-//import watchlist_icon from '../../assets/Watchlist-icon.png'; 
+//import watchlist_icon from '../../assets/Watchlist-icon.png';
 //import profile_icon from '../../assets/Profile-icon.png';
 
-import './Navbar.css';
-import Searchbar from './Searchbar.jsx';
+import "./Navbar.css";
+import Searchbar from "./Searchbar.jsx";
 
 function Navbar() {
   return (
     <nav>
-      <div className="logo">
-       
-      </div>
+      <div className="logo"></div>
 
       <ul className="nav-links">
         <li>
           <Link to="/homepage">
-            
             <span>Home</span>
           </Link>
         </li>
         <li>
-          <Link to="/all-movies"> 
-            
+          <Link to="/all-movies">
             <span>All Movies</span>
           </Link>
         </li>
         <li>
           <Link to="/profile">
-            
             <span>Profile</span>
           </Link>
         </li>
         <li>
           <Link to="/watchlist">
-            
-            <span>Watchlist</span> 
+            <span>Watchlist</span>
           </Link>
         </li>
         <li>
           <Link to="/rated-5-stars">
-            
             <span>Rated 5 Stars</span>
           </Link>
         </li>
@@ -55,13 +47,16 @@ function Navbar() {
       </div>
 
       <div className="auth-links">
-        <Link to="/login" className="btn">Login</Link>
+        <Link to="/login" className="btn">
+          Login
+        </Link>
         <hr />
-        <Link to="/signup" className="btn">Sign Up</Link>
+        <Link to="/signup" className="btn">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
 }
-
 
 export default Navbar;
